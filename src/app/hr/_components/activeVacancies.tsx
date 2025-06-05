f84ba0941a60d06072b028458eff6de6aee32931
Vanckoe <adil210@mail.ru>
2025-06-06 00:36:04 +0500

    fix(activeVacancies): correct text in vacancy prompt
    Remove redundant word "первую" (first) from the vacancy prompt text to make it more concise and natural

--- src/app/hr/_components/activeVacancies.tsx
+++ src/app/hr/_components/activeVacancies.tsx
@@ -8,7 +8,7 @@ const ActiveVacancies = () => {
             <p className="text-xl font-bold">Ваши активные вакансии</p>
             <div className="mt-4 rounded-[10px] bg-[#F8F8F8] px-12 py-10">
                 <p className="text-[32px] font-bold">
-                    Давайте добавим вашу <br /> первую вакансию
+                    Давайте добавим вашу <br /> вакансию
                 </p>
                 <Link href={'/hr/vacancy'}>
                     <Button className="mt-[50px] max-w-[310px]">Добавить вакансию</Button>
